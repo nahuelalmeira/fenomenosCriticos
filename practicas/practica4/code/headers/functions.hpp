3f096f6a27472e8bf3058508b661289124bebb96
nahuelalmeira <nahu1990@gmail.com>
2019-12-11 14:17:09 -0300

    Borro archivos viejos

--- practicas/practica4/code/headers/functions.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef FUNCTIONS_HPP
-#define FUNCTIONS_HPP
-
-double exponencial(double[], int);	//calcula exp(-dE/T)
-void pasoMC(int L, int **s, double *expVec, int *E, int *m);
-
-#endif // FUNCTIONS_HPP
